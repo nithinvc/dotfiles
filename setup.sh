34b8bf1c7ccac2f070e0cc888f258b2e39de8774
Nithin Chalapathi <nithin.ch10@gmail.com>
2025-05-22 16:58:07 -0700

    remote python and add github cli install

--- setup.sh
+++ setup.sh
@@ -37,8 +37,19 @@ echo 'alias u="cd .."' >> ~/.bashrc
 echo 'alias uu="cd ../../"' >> ~/.bashrc
 echo 'alias uuu="cd ../../../"' >> ~/.bashrc
 
-# Install gh
-conda install gh --channel conda-forge -y
+# Install gh NOTE: Theoretically this should be sufficient but it ran into deps issues?
+# conda install gh --channel conda-forge -y
+# Using this linux install instead
+
+(type -p wget >/dev/null || (sudo apt update && sudo apt-get install wget -y)) \
+	&& sudo mkdir -p -m 755 /etc/apt/keyrings \
+        && out=$(mktemp) && wget -nv -O$out https://cli.github.com/packages/githubcli-archive-keyring.gpg \
+        && cat $out | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg > /dev/null \
+	&& sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg \
+	&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+	&& sudo apt update \
+	&& sudo apt install gh -y
+
 
 # Install tmux
 sudo apt install tmux -y
@@ -54,8 +65,3 @@ mv init.lua ~/.config/nvim
 
 
 source ~/.bashrc
-
-# Python environment setup (eventually to abstract out)
-pip install ipython ipdb gpustat ruff graphviz
-pip install torch torchvision torchaudio
-pip install fire ruff open_clip_torch 
